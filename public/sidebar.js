87b3cf500d80fffeec5a9d93f42c121b82706b85
angelnetcast <angelnetcast@protonmail.com>
2025-08-16 03:13:32 -0500

    layout rework

---
+++ public/sidebar.js
@@ -0,0 +1,22 @@
+const sitenav = ` 
+<a href="/index.html">home</a> - about site - tutorials				
+`;
+
+document.getElementById("sitenav").innerHTML = sitenav;
+
+const sidebar = ` 
+<ul>
+	<li><a href="/pages/themed.html">themed layouts</a></li>
+	<li><a href="/pages/replica.html">replica layouts</a></li>
+	<li><a href="/pages/grids.html">grids</a></li>
+	<li><a href="/pages/navbars.html">navbars</a></li>
+</ul>				
+`;
+
+document.getElementById("sidebar").innerHTML = sidebar;
+
+const footer = ` 
+from <a href="https://angelnetcast.com">angel</a> with love				
+`;
+
+document.getElementById("footer").innerHTML = footer;
